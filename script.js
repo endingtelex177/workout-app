0318b67aa933cb7e7c9e9d7c4e43740f08b2eee4
endingtelex177 <160832438+endingtelex177@users.noreply.github.com>
2025-08-22 16:37:56 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let totalVal = [];
+let wakeLock = null;
+
+// Try to request wake lock
+async function requestWakeLock() {
+  try {
+    wakeLock = await navigator.wakeLock.request("screen");
+    console.log("Wake Lock is active");
+  } catch (err) {
+    console.error(`${err.name}, ${err.message}`);
+  }
+}
+
+// A button from 1 through 10 
+for (let i = 1;i <= 10; i++ ) {
+    const createButton = document.createElement("button");
+    createButton.innerText = `+${i}`;
+    createButton.id = `plus${i}`;
+    document.body.appendChild(createButton);
+
+    createButton.onclick = () => {
+        totalVal.push(i);
+        let total = totalVal.reduce((a,b)=> {
+            return a + b;
+        },0)
+        document.getElementById("display").innerText = total;
+    };
+}
+
+requestWakeLock();
+
