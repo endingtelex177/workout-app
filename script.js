0bda0815efd6d13f329d54e712ef0b82c4cf2dce
endingtelex177 <160832438+endingtelex177@users.noreply.github.com>
2025-08-25 14:39:15 +0800

    added button press and memory store after refresh

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let totalVal = [];
+if (localStorage.getItem("total") !== null){
+   totalVal = [Number(localStorage.getItem("total"))] 
+}
 let wakeLock = null;
 // get the localStorage value and display on screen
 let totalDisplay = localStorage.getItem("total");
 if (totalDisplay === null) {
   document.getElementById("display").innerText = 0;
-} else { 
+} else {
   document.getElementById("display").innerText = totalDisplay;
 }
 // Try to request wake lock
@@ -20,6 +23,19 @@ async function requestWakeLock() {
 const buttonContainer = document.createElement("div");
 buttonContainer.classList.add("button-container");
 document.body.appendChild(buttonContainer);
+
+
+const store = (x) => {
+  totalVal.push(x);
+  total = totalVal.reduce((a,b)=> {
+      return a + b;
+  },0)
+  
+  // set the total added value into a localStorage with a key "total"
+  localStorage.setItem("total",total);
+  totalDisplay = localStorage.getItem("total");
+  document.getElementById("display").innerText = totalDisplay;
+}
 //create 10 button, and put id on each of them, click of a button will return a value with the designated number
 for (let i = 1;i <= 10; i++ ) {
     const createButton = document.createElement("button");
@@ -27,17 +43,18 @@ for (let i = 1;i <= 10; i++ ) {
     createButton.id = `plus${i}`;
     buttonContainer.appendChild(createButton);
 
-    createButton.onclick = () => {
-        totalVal.push(i);
-        let total = totalVal.reduce((a,b)=> {
-            return a + b;
-        },0)
-        // set the total added value into a localStorage with a key "total"
-        localStorage.setItem("total",total);
-        totalDisplay = localStorage.getItem("total");
-        document.getElementById("display").innerText = totalDisplay;
+    let button = createButton.onclick = () => {
+    store(i);
     };
-}
+
+    document.addEventListener("keydown", function(event){
+      if (event.key === "0"){
+        store(1);
+      } else if (event.key === `${i}`){
+        button();
+      }
+    })};
+
 
 // reset button to 0 
 const resetButton = document.createElement("button");
